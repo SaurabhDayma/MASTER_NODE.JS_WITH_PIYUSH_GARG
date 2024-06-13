d138c7b107dc8963e009bb59695c718d1b54e48f
saurabh <saurabhdayma2019@gmail.com>
2024-06-13 16:13:09 +0530

    Modules in Node.js

---
+++ Module_Node.js/index.js
@@ -0,0 +1,17 @@
+// 1st code in node.js 
+
+ console.log("Namaste Node.js");
+
+//Modules in Node.js
+
+// Consider modules to be the same as JavaScript libraries.
+// A set of functions you want to include in your application.
+//To include a module, use the require() function with the name of the module
+
+
+//const app =  require('any module you want to use');
+
+
+
+
+
