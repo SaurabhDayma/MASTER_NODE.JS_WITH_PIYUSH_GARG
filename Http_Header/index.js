e19177e8632eb170425c64beeb8491543830033e
saurabh <saurabhdayma2019@gmail.com>
2024-06-20 12:13:25 +0530

    commit

---
+++ Http_Header/index.js
@@ -0,0 +1,46 @@
+const express =  require("express");
+const fs = require("fs");
+
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+
+app.use(express.urlencoded({express : false}));
+
+app.use((req, res, next) => {
+    fs.appendFile("./log.txt" , `${Date.now()} : ${req.method} : ${req.path}` , (err,data) => {
+        next();
+    });
+})
+
+app.get('/api/users', (req,res)=>{
+    const html = `
+       <ul>
+         ${users.map((user) => `<li>${user.first_name}</li>`)}
+       </ul>
+        `
+        res.send(html);
+})
+
+app.get('/users' , (req,res) => {
+    res.setHeader("x-Myheader", "Saurabh_Dayma");
+    console.log(req.headers);
+    return res.json(users);
+}) 
+
+
+app.post('/api/users', (req,res) => {
+     const body =  req.body;
+     users.push({id : users.length +1 , ...body});
+     fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users), (err,data) =>{
+        return res.json({status : "Sucesss", id: users.length}) 
+     })
+
+})
+
+
+const PORT = 8000;
+
+app.listen(PORT, () =>{
+    console.log(`Server Started at Port ${PORT}`);
+})
